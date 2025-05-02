1f262186cdf08966568f59af1cac24750cf3411c
DVanam <hisokadara@gmail.com>
2025-05-02 17:19:35 +0300

    create requerment.txt redact-venv-main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
 import sys
+import requests
 
 python_path = os.path.dirname(sys.executable)
 
 python_version = sys.version
 
+def main():
+  return requests.get(url='https://google.com')
+
 # В терминал .\main.py
 print(f'ПИТЬ К ИНТЕРПРЕТАТОРУ {python_path}. ВЕРСИЯ {python_version}')
+
+if __name__ == '__main__':
+  print(main())
