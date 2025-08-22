6e5f306467a473e2856266b64efb4f8b7f575468
ADenvan <hisokadara@gmail.com>
2025-08-22 22:08:26 +0300

    ДОбавлен settings.json + venv-exe.txt Обновлен + Добавлен фаил py-cmd.py

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import os
 import sys
 import requests
 
+# Путь к интерпритатору
 python_path = os.path.dirname(sys.executable)
-
+# Версия python
 python_version = sys.version
 
+
 def main():
-  return requests.get(url='https://google.com')
+    return requests.get(url="https://google.com")
+
 
 # В терминал .\main.py
-print(f'ПИТЬ К ИНТЕРПРЕТАТОРУ {python_path}. ВЕРСИЯ {python_version}')
-
-if __name__ == '__main__':
-  print(main())
+print("...")
+print(f"ПИТЬ К ИНТЕРПРЕТАТОРУ {python_path}. \nВЕРСИЯ {python_version}")
+if __name__ == "__main__":
+    print(main())
